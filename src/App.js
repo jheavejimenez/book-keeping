1fc03b67839999f324e6ef75ca17e330677b55d7
Charlon-Dale <charlondalesanagustin@gmail.com>
2022-09-12 22:57:52 +0800

    added button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Input from "./components/Input/Input";
+import Button from "./components/Button/Button";
 
 function App() {
   return (
     <div className="App">
       <Input placeHolder={"username"} type={"text"} />
+      <Input placeHolder={"password"} type={"password"} />
+      <Button text={"login"} />
     </div>
   );
 }
