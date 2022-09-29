3ffae58c3a54bbae670566ba784b3e0d7f15d65e
jheave <jimenezjheave123@gmail.com>
2022-09-29 19:53:49 +0800

    added pencil icon

--- src/components/Table/TableRow.jsx
+++ src/components/Table/TableRow.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { PencilSquareIcon } from "@heroicons/react/24/outline";
 
 function TableRow({ DocID, SenderName, fileName, timeStamp, status }) {
     let colors;
@@ -32,12 +33,7 @@ function TableRow({ DocID, SenderName, fileName, timeStamp, status }) {
                 </span>
             </td>
             <td className={"flex justify-center items-center w-14 h-14 transition-all duration-300 transform group-hover:rotate-12"}>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#00A2E8" class="w-6 h-6">
-                    <path
-                        d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
-                    <path
-                        d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
-                </svg>
+               <PencilSquareIcon className={"w-6 h-6 text-blue-500 group-hover:text-blue-700"}/>
             </td>
 
         </tr>
