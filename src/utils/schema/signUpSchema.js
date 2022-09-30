e7bea19515202c18bcfc5fce86cea23e918c1285
jheave <jimenezjheave123@gmail.com>
2022-09-30 22:33:30 +0800

    add schema validation for signUp

--- src/utils/schema/signUpSchema.js
+++ src/utils/schema/signUpSchema.js
@@ -1,7 +1,14 @@
 import * as Yup from 'yup';
 
 export const SignupSchema = Yup.object().shape({
-    email: Yup.string().email('Invalid email').required('Required'),
+    email: Yup.string()
+        // check if is a valid email that contains @ and .
+        .matches(
+            /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            'Invalid email address'
+        )
+        .required('Required'),
+
     password: Yup.string()
         .min(6, 'Too Short!')
         // password must contain at least one special character and one number and one uppercase letter
@@ -9,5 +16,6 @@ export const SignupSchema = Yup.object().shape({
             /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{6,})/,
             'Password must contain at least one special character and one number and one uppercase letter'
         )
-        .password('Invalid password').required('Required'),
+        .password('Invalid password')
+        .required('Required'),
 });
