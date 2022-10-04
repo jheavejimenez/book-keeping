955f2bc26047cc4377969ebb11271b3daf8a0ff4
daldumalay <darell@xperto.ph>
2022-10-04 21:24:43 +0800

    *Centered Image in Account Settings *Centered Verify Email

--- src/pages/Auth/VerifyEmail.jsx
+++ src/pages/Auth/VerifyEmail.jsx
@@ -12,24 +12,24 @@ function VerifyEmail() {
     }
 
     return (
-        <div className='flex justify-center mt-9'>
-            <div className={"bg-slate-200 border-2 border-solid rounded-md border-sky-200 p-5"}>
-                <div className={""}>
-                    <h1 className={"text-2xl font-bold tracking-wide mb-6"}>Verify your Email Address</h1>
-                    <div className={"mb-6"}>
-                        <p className={"mb-2"}>
-                            <strong >A Verification email has been sent to:</strong><br />
-                        </p>
-                        <span>Follow the instruction in the email to verify your account</span>
-                    <div className={"flex justify-end mr-6"}>
-                        <button onClick={resendEmailVerification} className={"bg-blue-300 rounded p-2 mt-9 hover:bg-blue-400 active:bg-blue-400 font-semibold"}>
-                            Resend Email
-                        </button>
-                    </div>
-                    </div>
+    <div className='flex min-h-screen items-center justify-center'>
+        <div className={"bg-slate-200 border-2 border-solid rounded-md border-sky-200 p-5 items-center"}>
+            <div>
+                <h1 className={"text-2xl font-bold tracking-wide mb-6"}>Verify your Email Address</h1>
+                <div className={"mb-6"}>
+                    <p className={"mb-2"}>
+                        <strong >A Verification email has been sent to:</strong><br />
+                    </p>
+                    <span>Follow the instruction in the email to verify your account</span>
+                <div className={"flex justify-end mr-6"}>
+                    <button onClick={resendEmailVerification} className={"bg-blue-300 rounded p-2 mt-9 hover:bg-blue-400 active:bg-blue-400 font-semibold"}>
+                        Resend Email
+                    </button>
+                </div>
                 </div>
             </div>
         </div>
+    </div>
     )
 }
 
