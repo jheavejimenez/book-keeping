10d4fefc3727c10a5a934824d982fd852288f461
Charlon-Dale <charlondalesanagustin@gmail.com>
2022-10-18 07:44:01 +0800

    fix code style for ChangeStatus.jsx

--- src/components/Button/ChangeStatus.jsx
+++ src/components/Button/ChangeStatus.jsx
@@ -17,7 +17,8 @@ function ChangeStatus() {
             {showModal && (
                 <>
                     <div
-                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                        className={"justify-center items-center flex overflow-x-hidden overflow-y-auto" + 
+                        "fixed inset-0 z-50 outline-none focus:outline-none"}>
                         <div className="relative w-1/4 my-6 md:w-2/3 mx-auto max-w-md ">
                             {/*content*/}
                             <div className={"flex justify-center border-0 rounded-lg " +
@@ -26,13 +27,17 @@ function ChangeStatus() {
                             >
                                 {/*header*/}
                                 <div
-                                    className="flex items-center sm:items-start p-5 border-b border-solid border-slate-200 rounded-t">
+                                    className={"flex items-center sm:items-start p-5" + 
+                                    "border-b border-solid border-slate-200 rounded-t"}
+                                >
                                     <h3 className="text-2xl font-bold text-black">
                                         Edit Status
                                     </h3>
+
                                     <button
-                                        className={"p-1 ml-auto text-gray-400 hover:text-black opacity-50 float-right text-3xl" + 
-                                        "leading-none font-semi-bold outline-none focus:outline-none"}
+                                        className={"p-1 ml-auto text-gray-400 hover:text-black opacity-50" + 
+                                        "float-right text-3xl leading-none font-semi-bold outline-none" + 
+                                        "focus:outline-none"}
                                         onClick={() => setShowModal(false)}
                                     >
                                         <XMarkIcon className="w-7 h-7" />
@@ -40,17 +45,25 @@ function ChangeStatus() {
                                 </div>
                                 {/*body*/}
                                 <div
-                                    className="flex space-y-6 px-6 lg:px-6 pb-4 sm:pb-6 xl:pb-6 items-center justify-center p-5">
+                                    className={"flex space-y-6 px-6 lg:px-6 pb-4 sm:pb-6 xl:pb-6" + 
+                                    "items-center justify-center p-5"}
+                                    >
                                     <form className={"relative space-y-3.5 w-auto shrink"}>
-                                        <label className={"font-bold mr-2"}> Progress Status </label>
-                                        <ChooseStatusDropdown className={"relative space-y-3.5 md:w-auto shrink"} />
+                                        <label className={"font-bold mr-2"}> 
+                                            Progress Status 
+                                        </label>
+
+                                        <ChooseStatusDropdown className={"relative space-y-3.5" + 
+                                        "md:w-auto shrink"} />
                                     </form>
                                 </div>
                                 {/*footer*/}
                                 <div className="flex items-center justify-center p-2">
                                     <button
-                                        className={"text-blue-500 background-transparent font-bold uppercase px-6 py-2 text-sm" + 
-                                        "outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"}
+                                        className={"text-blue-500 background-transparent font-bold" + 
+                                        "uppercase px-6 py-2 text-sm" + 
+                                        "outline-none focus:outline-none mr-1 mb-1 ease-linear" + 
+                                        "transition-all duration-150"}
                                         type="button"
                                         onClick={() => setShowModal(false)}
                                     >
