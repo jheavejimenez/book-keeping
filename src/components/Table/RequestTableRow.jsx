0bf21295b20c94b1dbb422d2ac723c89881e9a21
jheave <jimenezjheave123@gmail.com>
2022-10-17 22:53:05 +0800

    ad query to request documents

--- src/components/Table/RequestTableRow.jsx
+++ src/components/Table/RequestTableRow.jsx
@@ -1,43 +1,32 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import { sendEmailVerification, signInWithEmailAndPassword, getAuth, onAuthStateChanged  } from 'firebase/auth'
-import { collection, doc, setDoc, getDocs, query, where } from "firebase/firestore"; 
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../utils/Firebase";
 
-function RequestTableRow({ReqID, RequestedFrom, File, Purpose, DueDate, DateRequested}) {
-    const [reqfrom , setReqfrom] = useState('')
-    const [file , setFile] = useState('')
-    const [dueDate , setDueDate] = useState('')
-    const [reqby , setReqby] = useState('')
-    const [purpose , setPurpose] = useState('')
-    const [dateReq , setDateReq] = useState('')
+function RequestTableRow() {
+    const [data, setData] = useState([]);
+
+    const getAllRequestDocumments = async () => {
+        const snapshot = await getDocs(collection(db, "request"));
+        setData(snapshot.docs.map((doc) => doc.data()));
+    }
 
     useEffect(() => {
-        const q = query(collection(db, "request"), where("ReqID", "==", ReqID));
-        getDocs(q).then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                setReqfrom(doc.data().name)
-                console.log(doc.data().name)
-            });
-        });
-        setFile(File)
-        setPurpose(Purpose)
-        setDueDate(DueDate)
-        setDateReq(DateRequested)
-        setReqby(RequestedFrom)
-        console .log(ReqID)
-    }, [RequestedFrom, File, Purpose, DueDate, DateRequested])
-        
-    
-
+        getAllRequestDocumments();
+        const interval = setInterval(() => {
+            getAllRequestDocumments();
+        }, 10000)
+        return () => {
+            clearInterval(interval); // need to clear the interval when the component unmounts to prevent memory leaks
+        };
+    }, []);
+    console.log(data);
     return (
         <tr className={"hover:bg-gray-300 text-black"}>
-            <td className={"px-4 py-3 text-sm"}>{ReqID}</td>
-            <td className={"px-4 py-3 text-sm"}>{RequestedFrom}</td>
-            <td className={"px-4 py-3 text-sm"}>{File}</td>
-            <td className={"px-4 py-3 text-xs"}>{Purpose}</td>
-            <td className={"px-4 py-3 text-xs"}>{DueDate}</td>
-            <td className={"px-4 py-3 text-xs"}>{DateRequested}</td>    
+            {/*{data.map((item) => (*/}
+            {/*    <td className={"px-4 py-3 text-sm"}>*/}
+            {/*        {item.}*/}
+            {/*    </td>*/}
+            {/*))}*/}
         </tr>
     )
 }
