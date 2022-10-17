f201fb11138352dc2c8c69c0e17a3d7b13afb923
jheave <jimenezjheave123@gmail.com>
2022-10-17 22:59:13 +0800

    refactor

--- src/components/Table/RequestTable.jsx
+++ src/components/Table/RequestTable.jsx
@@ -1,36 +1,28 @@
 import React, { useEffect, useState } from "react";
 import RequestTableRow from "./RequestTableRow";
-import axios from "axios";
 import TableHeading from "./TableHeading";
 import Pagination from "../Pagination/Pagination";
+import { collection, getDocs } from "firebase/firestore";
 
 function RequestTable() {
     const [data, setData] = useState([]);
     const [titleTable, setTitleTable] = useState([]);
 
-    const fakeData = async () => {
-        const response = await axios.get("http://localhost:3000/requestData");
-        setData(response.data);
-    }
-
-    const fakeTitleTable = async () => {
-        const response = await axios.get("http://localhost:3000/requestHeader");
-        setTitleTable(response.data[0].title);
-
+    const getAllRequestDocumments = async () => {
+        const snapshot = await getDocs(collection(db, "request"));
+        setData(snapshot.docs.map((doc) => doc.data()));
     }
 
     useEffect(() => {
-        fakeData();
-        fakeTitleTable();
+        getAllRequestDocumments();
         const interval = setInterval(() => {
-            fakeData();
-            fakeTitleTable();
+            getAllRequestDocumments();
         }, 10000)
         return () => {
             clearInterval(interval); // need to clear the interval when the component unmounts to prevent memory leaks
         };
     }, []);
-
+    console.log(data);
 
     return (
         <>
@@ -62,7 +54,7 @@ function RequestTable() {
                             </tbody>
                         </table>
                     </div>
-                    <Pagination/>
+                    <Pagination />
                 </div>
             </div>
         </>
--- src/components/Table/RequestTableRow.jsx
+++ src/components/Table/RequestTableRow.jsx
@@ -3,30 +3,18 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../utils/Firebase";
 
 function RequestTableRow() {
-    const [data, setData] = useState([]);
-
-    const getAllRequestDocumments = async () => {
-        const snapshot = await getDocs(collection(db, "request"));
-        setData(snapshot.docs.map((doc) => doc.data()));
-    }
-
-    useEffect(() => {
-        getAllRequestDocumments();
-        const interval = setInterval(() => {
-            getAllRequestDocumments();
-        }, 10000)
-        return () => {
-            clearInterval(interval); // need to clear the interval when the component unmounts to prevent memory leaks
-        };
-    }, []);
-    console.log(data);
     return (
         <tr className={"hover:bg-gray-300 text-black"}>
-            {/*{data.map((item) => (*/}
-            {/*    <td className={"px-4 py-3 text-sm"}>*/}
-            {/*        {item.}*/}
-            {/*    </td>*/}
-            {/*))}*/}
+            {data.map((item) => (
+                <>
+                    <td className={"px-4 py-3 text-sm"}>
+                        {item.documentId}
+                    </td>
+                    <td className={"px-4 py-3 text-sm"}>
+                        {item.reqfrom}
+                    </td>
+                </>
+            ))}
         </tr>
     )
 }
