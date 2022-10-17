f13526a60c8fe1689805c9e4f9598500fe0ccfb7
jheave <jimenezjheave123@gmail.com>
2022-10-17 17:41:57 +0800

    refactro class --> className

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -6,7 +6,7 @@ function Alert({alert}) {
             <div className={"bg-red-500 text-white font-bold rounded-t px-4 py-2"}>
                 Error
             </div>
-            <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+            <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
                 <p>{alert}</p>
             </div>
         </div>
