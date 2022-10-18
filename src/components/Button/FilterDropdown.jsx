f55c878f2c490eaeb97de80761bc390558264132
Charlon-Dale <charlondalesanagustin@gmail.com>
2022-10-18 08:16:09 +0800

    fix code style

--- src/components/Button/FilterDropdown.jsx
+++ src/components/Button/FilterDropdown.jsx
@@ -9,8 +9,10 @@ function FilterDropdown() {
         <Menu as="div" className="relative inline-block text-left">
 
             <div>
-                <Menu.Button className={"inline-flex w-full justify-center w-16 rounded-md border border-gray-300 " +
-                    "bg-white px-2 py-2 text-sm font-medium text-gray-400 shadow-sm hover:bg-gray-50 focus:outline-none " +
+                <Menu.Button className={"inline-flex w-full justify-center w-16" + 
+                    "rounded-md border border-gray-300 " +
+                    "bg-white px-2 py-2 text-sm font-medium text-gray-400 shadow-sm hover:bg-gray-50" + 
+                    "focus:outline-none " +
                     "focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100"}>
                     All
                     <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
--- src/components/Button/RequestButton.jsx
+++ src/components/Button/RequestButton.jsx
@@ -55,16 +55,19 @@ function RequestButton({ text }) {
                         <div className="container mx-auto w-11/12 md:w-2/3 max-w-md">
                             {/*content*/}
                             <div
-                                className="relative py-6 px-6 md:px-6 bg-white shadow-md rounded border border-gray-400">
+                                className={"relative py-6 px-6 md:px-6 bg-white shadow-md rounded border" +
+                                "border-gray-400"}>
                                 {/*header*/}
                                 <div
-                                    className="flex items-start justify-between pb-3 border-b border-solid border-slate-200 rounded-t-md">
+                                    className={"flex items-start justify-between pb-3 border-b border-solid" + 
+                                    "border-slate-200 rounded-t-md"}>
                                     <h3 className="text-2xl font-semi-bold text-black">
                                         Request a File
                                     </h3>
                                     <button
-                                        className={"p-1 ml-auto text-gray-400 hover:text-black opacity-50 float-right" + 
-                                        "text-3xl leading-none font-semi-bold outline-none focus:outline-none"}
+                                        className={"p-1 ml-auto text-gray-400 hover:text-black opacity-50" + 
+                                        "float-right text-3xl leading-none font-semi-bold outline-none" + 
+                                        "focus:outline-none"}
                                         type="button"
                                         onClick={() => setShowModal(false)}
                                     >
@@ -78,31 +81,35 @@ function RequestButton({ text }) {
                                             <div>
                                                 <label htmlFor="reqFrom" className={"text-black"}>Request From</label>
                                                 <input id="reqFrom"
-                                                       className={"border rounded-md mb-3 mt-1 h-10 pl-3 border-gray-400 font-normal " +
-                                                           "placeholder-gray-400 text-black text-base w-full"}
+                                                       className={"border rounded-md mb-3 mt-1 h-10 pl-3" + 
+                                                       "border-gray-400 font-normal placeholder-gray-400"+ 
+                                                       "text-black text-base w-full"}
                                                        placeholder="Client/User's Company Email" type="email"
                                                        onChange={(e) => setReqfrom(e.target.value)} />
                                             </div>
                                             <div>
                                                 <label htmlFor="fileName" className={"text-black"}>Filename</label>
                                                 <input id="fileName"
-                                                       className={"border rounded-md mb-3 mt-1 h-10 pl-3 border-gray-400 font-normal " +
-                                                           "placeholder-gray-400 text-black text-base w-full"}
+                                                       className={"border rounded-md mb-3 mt-1 h-10" + 
+                                                       "pl-3 border-gray-400 font-normal placeholder-gray-400"+
+                                                       "text-black text-base w-full"}
                                                        placeholder="Transactions.xlsx / .pdf"
                                                        onChange={(e) => setFile(e.target.value)} />
                                             </div>
                                             <div>
                                                 <label htmlFor="dueDate" className={"text-black"}>Set Due Date</label>
                                                 <input id="dueDate"
-                                                       className={"w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
-                                                           + "placeholder-gray-400 text-black text-base w-full"}
+                                                       className={"w-full px-4 py-2 mt-2 border rounded-md" + 
+                                                       "focus:outline-none focus:ring-1 focus:ring-blue-600" +
+                                                        "placeholder-gray-400 text-black text-base w-full"}
                                                        type="date" onChange={(e) => setDueDate(e.target.value)} />
                                             </div>
                                             <div>
                                                 <label htmlFor="reqBy" className={"text-black"}>Requested By</label>
                                                 <input id="reqBy"
-                                                       className={"border rounded-md mb-3 mt-1 h-10 pl-3 border-gray-400 font-normal " +
-                                                           "placeholder-gray-700 text-black text-base w-full"}
+                                                       className={"border rounded-md mb-3 mt-1 h-10 pl-3" + 
+                                                       "border-gray-400 font-normal" +
+                                                       "placeholder-gray-700 text-black text-base w-full"}
                                                        placeholder={reqby} disabled />
                                             </div>
                                             <div>
@@ -114,8 +121,9 @@ function RequestButton({ text }) {
                                                 </label>
                                                 <textarea
                                                     id="purpose" rows={2}
-                                                    className={"block p-2.5 w-full text-base font-normal text-gray-900 rounded-lg border " +
-                                                        "border-gray-400 focus:ring-blue-500 focus:border-blue-500 "}
+                                                    className={"block p-2.5 w-full text-base font-normal"+
+                                                    "text-gray-900 rounded-lg border border-gray-400" + 
+                                                    "focus:ring-blue-500 focus:border-blue-500"}
                                                     placeholder="Your purpose..."
                                                     onChange={(e) => setPurpose(e.target.value)}
                                                 />
@@ -125,18 +133,22 @@ function RequestButton({ text }) {
                                 </div>
                                 {/*footer*/}
                                 <div
-                                    className="flex items-center justify-end pt-6 border-t border-solid border-slate-200 rounded-b-md">
+                                    className={"flex items-center justify-end pt-6 border-t border-solid"+
+                                    "border-slate-200 rounded-b-md"}>
                                     <button
-                                        className={"text-gray-500 hover:text-blue-500 background-transparent font-bold uppercase px-6 py-2 text-sm " +
-                                            "outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"}
+                                        className={"text-gray-500 hover:text-blue-500 background-transparent" + 
+                                        "font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none" + 
+                                        "mr-1 mb-1 ease-linear transition-all duration-150"}
                                         type="button"
                                         onClick={() => setShowModal(false)}
                                     >
                                         Close
                                     </button>
                                     <button
-                                        className={"bg-blue-500 hover:bg-blue-400 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 " +
-                                            "rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"}
+                                        className={"bg-blue-500 hover:bg-blue-400 text-white active:bg-emerald-600"
+                                        + "font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg" + 
+                                        "outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all" +
+                                        "duration-150"}
                                         type="button"
                                         onClick={handleSubmit}
                                     >
