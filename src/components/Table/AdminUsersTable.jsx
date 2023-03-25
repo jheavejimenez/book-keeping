a3bab4884a4c264d5858d80495e6306eebee3b17
LPxperto <lawrence@xperto.ph>
2023-03-25 20:59:05 +0800

    fix results

--- src/components/Table/AdminUsersTable.jsx
+++ src/components/Table/AdminUsersTable.jsx
@@ -64,7 +64,7 @@ function AdminUsersTable() {
     const auditTrailCollectionRef = collection(db, "audittrail",);
    
     const fetchData = async () => {
-        const q = query(collection(db, "users"),orderBy("email", "desc"), limit(5));
+        const q = query(collection(db, "users"),orderBy("email", "asc"), limit(5));
         const querySnapshot = await getDocs(q)
         const items = []
         querySnapshot.forEach((doc) => {
